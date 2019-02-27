be92cebaaeba86528bf8e4d4e7e7ae57d253759a
ron ucitel <ron.ucitel@hotmail.com>
2019-02-27 13:13:04 +0200

    v0.3

--- MyTCPClient/ChatForm.cs
+++ MyTCPClient/ChatForm.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace MyTCPClient
 {
     public partial class ChatForm : Form
     {
+        Thread t;
+        TcpClient client;
         IPAddress serverIP;
         int port;
-        string name;
+        string name, friend = "friend";
+        NetworkStream stream;
         public ChatForm(IPAddress IP, int Port, string Name)
         {
             InitializeComponent();
@@ -18,39 +23,62 @@ namespace MyTCPClient
             serverIP = IP;
             port = Port;
             name = Name;
-            Mtb.MaxLength = 256;
-            Connect("test");
+            Mtb.MaxLength = 255;
+            t = new Thread(Recive);
+            Connect();
         }
 
         private void Sendb_Click(object sender, EventArgs e)
         {
-            Connect(Mtb.Text);
+            Send(Mtb.Text);
         }
 
         private void ChatForm_FormClosing(object sender, FormClosingEventArgs e)
         {
+            // Close everything.
+            stream.Close();
+            client.Close();
             Application.Exit();
         }
 
-        void Connect(string message)
+        void Connect()
         {
             try
             {
                 //TcpClient client = new TcpClient(server, port);
-                TcpClient client = new TcpClient();
+                client = new TcpClient();
                 client.Connect(serverIP, port);
-                // Translate the passed message into ASCII and store it as a Byte array.
-                byte[] data = Encoding.ASCII.GetBytes(message);
+
+                // Translate the passed name into ASCII and store it as a Byte array.
+                byte[] data = Encoding.ASCII.GetBytes("~" + name);
 
                 // Get a client stream for reading and writing.
-                //  Stream stream = client.GetStream();
+                stream = client.GetStream();
 
-                NetworkStream stream = client.GetStream();
+                // Send the name to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+            }
+            catch (ArgumentNullException e)
+            {
+                Dtb.Text += ("\nArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Dtb.Text += ("\nSocketException: {0}", e);
+            }
+            t.Start();
+        }
+
+        public bool Send(string message)
+        {
+            try
+            {
+                // Translate the passed message into ASCII and store it as a Byte array.
+                byte[] data = Encoding.ASCII.GetBytes(message);
 
                 // Send the message to the connected TcpServer. 
                 stream.Write(data, 0, data.Length);
 
-                Dtb.Text += ("\n{name}: {0}", message);
 
                 // Receive the TcpServer.response.
 
@@ -63,22 +91,60 @@ namespace MyTCPClient
                 // Read the first batch of the TcpServer response bytes.
                 int bytes = stream.Read(data, 0, data.Length);
                 responseData = Encoding.ASCII.GetString(data, 0, bytes);
-                Dtb.Text += ("\nReceived: {0}", responseData);
-
-                // Close everything.
-                stream.Close();
-                client.Close();
+                Dtb.Text += ("\n{friend}: {0}", responseData);
             }
             catch (ArgumentNullException e)
             {
                 Dtb.Text += ("\nArgumentNullException: {0}", e);
+                return false;
             }
             catch (SocketException e)
             {
                 Dtb.Text += ("\nSocketException: {0}", e);
+                return false;
             }
+            return true;
+        }
 
-            Dtb.Text += ("\n Press Enter to continue...");
+        public void Recive()
+        {
+            while (true)
+            {
+                try
+                {
+                    // Receive the TcpServer.response.
+
+                    // Buffer to store the response bytes.
+                    byte[] data = new byte[256];
+
+                    // String to store the response ASCII representation.
+                    string responseData = string.Empty;
+
+                    // Read the first batch of the TcpServer response bytes.
+                    int bytes = stream.Read(data, 0, data.Length);
+                    responseData = Encoding.ASCII.GetString(data, 0, bytes);
+                    if (responseData.First() == '~')
+                    {
+                        friend = responseData.Split('~')[1];
+                    }
+                    else if (responseData.First() == '-')
+                    {
+                        friend = responseData.Split('-')[1];
+                    }
+                    else if (responseData.First() == '?')
+                    {
+                        Send("~" + name);
+                    }
+                }
+                catch (ArgumentNullException e)
+                {
+                    Dtb.Text += ("\nArgumentNullException: {0}", e);
+                }
+                catch (SocketException e)
+                {
+                    Dtb.Text += ("\nSocketException: {0}", e);
+                }
+            }
         }
     }
 }
